357731c6e50277303a00d0357d5b8d7a8e834052
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 12:03:15 -0800

    Update

--- shared-memory.cpp
+++ shared-memory.cpp
@@ -1,6 +1,7 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <cstdio>
 #include <string>
 #include <cstdlib>
 
@@ -8,11 +9,14 @@ using namespace std;
 
 string cleanFilename(string filename)
 {
-	return realpath(filename);
+	char* output = realpath(filename, new char[PATH_MAX+1]);
+	string out = output;
+	delete output;
+	return out;
 }
 
 // Always returns a shared memory file descriptor
-int getSharedMemory(string filename)
+BufferContents* getSharedMemory(string filename)
 {
 	string name = "";
 	for (char letter : cleanFilename(filename)) {
@@ -22,24 +26,57 @@ int getSharedMemory(string filename)
 			name += letter;
 	}
 
-	// BUFFER CONTENTS
-	// size_t \0 int int ... \0 <CONTENTS>
-		size_t size;
-		
-	};
-	// <SIZE> \0 <CURSOR1> <CURSOR2> ... \0 <CONTENTS>
+	// CONTENT
+	// int \0 int int ... \0 void*
+	// <NUMBER OF CURSORS> \0 <CURSOR1> <CURSOR2> ... \0 <CONTENTS>
 
-	const int SIZE = 524288;
+
+	// SIZE
+	// unsigned long long
+	const int SIZE_SIZE = 524288;
+	const char* SIZE_NAME = "SIZE_" + name;
 	const char* NAME = name;
+	BufferContents* bc;
+	BufferContents bufferContents;
 
-	if (shm_open(SIZE_NAME, O_RDONLY, 0666) >= 0) {
-		int shm_size = shm_open(SIZE_NAME, O_RDONLY, 0666);
-		void* ptr = mmap(0, SIZE_SIZE, PROT_READ, MAP_SHARED, shm_size, 0);
-		printf("%s"
+	int shm_size = shm_open(SIZE_NAME, O_RDONLY, 0666);
+	int shm = shm_open(NAME, O_RDONLY, 0666);
+	if (shm_size >= 0) {
+		bc = (BufferContents*)mmap(0, SIZE_SIZE, PROT_READ, MAP_SHARED, shm_size, 0);
+		bufferContents.size = bc->size;
+		shm = shm_open(NAME, O_CREAT | O_RDONLY, 0666);
+		return (BufferContents*)mmap(0, bc->size, PROT_WRITE, MAP_SHARED, shm, 0);
+	} else {
+		// Must make new everything
 	}
 
 	// Look inside temp for that file
 		// Yes - Open that one
 		// No - Make a new one
 	shm_unlink(name);
+}
+
+/*	struct BufferContents {
+		unsigned long long size;
+		int numberOfCursors;
+		int[] cursorPosition;
+		void* content;
+	};*/
+
+void parseBufferContents(BufferContents bufferContents, unsigned long long &size, int &numberOfCursors, int[] cursorPosition, void* content)
+{
+	size = bufferContents.size;
+	numberOfCursors = bufferContents.numberOfCursors;
+	cursorPosition = bufferContents.cursorPosition;
+	content = bufferContents.content;
+}
+
+BufferContents readBufferConents(BufferContents &bufferContents, unsigned long long size, int numberOfCursors, int[] cursorPosition, void* content)
+{
+	BufferContents bc;
+	bc.size = size;
+	bc.numberOfCursors = numberOfCursors;
+	bc.cursorPosition = cursorPosition;
+	bc.content = content;
+	return bc;
 }
--- shared-memory.h
+++ shared-memory.h
@@ -4,17 +4,20 @@
 	#define SHARED_MEMORY_H
 
 	// Always returns a shared memory file descriptor
-	int getSharedMemory(std::string filename);
+	BufferContents* getSharedMemory(std::string filename);
 
 	struct BufferContents {
 		unsigned long long size;
 		int numberOfCursors;
-		int[] cursorPosition;
 		void* content;
+		int[] cursorPosition;
 	};
 
-	BufferContents parseBufferContents(unsigned long long &size, int &numberOfCursors, int[] cursorPosition, void* content);
-	void fillBufferConents(BufferContents &bufferContents, unsigned long long size, int numberOfCursors, int[] cursorPosition, void* content);
+	// Fill arguments from BufferContents
+	void parseBufferContents(BufferContents bufferContents, unsigned long long &size, int &numberOfCursors, int[] cursorPosition, void* content);
+
+	// Fill BufferContents from arguments
+	BufferContents readBufferConents(BufferContents &bufferContents, unsigned long long size, int numberOfCursors, int[] cursorPosition, void* content);
 
 	/*
 		Opening a file creates /tmp/vim-with-friends/.../
