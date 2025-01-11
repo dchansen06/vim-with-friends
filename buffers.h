845e735d9accbf2961cc4c36f1ab9878380420dc
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 10:18:27 -0800

    Add file

---
+++ buffers.h
@@ -0,0 +1,20 @@
+#ifndef BUFFERS_H
+	#define BUFFERS_H
+
+
+	/* Server
+		R/W disk
+		Listens to clients
+	Client
+		R/W file
+		If given a file, then start server
+
+	Message recieved flag
+	Queue holding function
+	Send ack for every recieved */
+
+	/*
+		Share buffer memory between processes
+		Everyone R/W same memory
+	*/
+#endif
