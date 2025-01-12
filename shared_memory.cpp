7ebdd47cfc49883454f560d40863340aba20b1c0
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 16:33:52 -0800

    U

--- shared_memory.cpp
+++ shared_memory.cpp
@@ -38,23 +38,15 @@ volatile BufferContents* getSharedMemory(string filename, bool &host)
 	const int SIZE = 524288;
 	const char* NAME = ("/tmp" + name).c_str();	// Later mkdir so that we can /tmp/vim-with-friends/
 
-<<<<<<< HEAD
-	int shm = shm_open(NAME, O_EXCL|O_CREAT, 0666);
-=======
-	int shm = shm_open(NAME, O_RDWR, 0666);
+	int shm = shm_open(NAME, O_EXCL|O_CREAT|O_RDWR, 0666);
 	cout << "SHM: " << shm << " should be >=0 for client\n";
->>>>>>> 6c194198670781fc9af5a0b0d4047fee1a54d612
 	if (shm >= 0) {
-		shm_unlink(NAME);
-
 		host = false;
-		shm = shm_open(NAME, O_RDWR, 0666);
-
 		return (volatile BufferContents*)mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm, 0);
 	} else {
 		host = true;
 
-		shm = shm_open(NAME, O_CREAT | O_RDWR, 0666);
+		shm = shm_open(NAME, O_EXCL|O_CREAT|O_RDWR, 0666);
 		ftruncate(shm, SIZE);
 
 		if (shm >= 0)
