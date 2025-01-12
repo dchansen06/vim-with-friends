458c271b3073d7520666937dff91773694b61810
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 21:29:25 -0800

    U

--- editor.cpp
+++ editor.cpp
@@ -1,7 +1,60 @@
+#include <cctype>
+
+#include <ncurses.h>
+
 #include "editor.h"
 
-#ifndef EDITOR_H
-	#define EDITOR_H
+void insert(char character, BufferContents *bufferContent)
+{
 
-	void update(char input);
-#endif
+}
+
+void update(char input, BufferContents *bufferContent, int cursorIdentity)
+{
+	switch(input) {
+		case KEY_UP:
+			
+			break;
+		case KEY_DOWN:
+			int currentPositionX = 0;
+
+			while (currentPositionX < bufferContent->size - bufferContent->cursorPos[cursorIdentity] && bufferContent->content[bufferContent->cursorPos[cursorIdentity] - currentPositionX] != '\n')
+				currentPositionX++;
+
+			while (countToHome != 0 && bufferContent->content[countToHome] != '\n') {
+				cursorPos++;
+				countToHome--;
+			}
+
+			break;
+		case KEY_LEFT:
+			cursorPos[cursorIdentity]--;
+			break;
+		case KEY_RIGHT:
+			cursorPos[cursorIdentity]++;
+			break;
+		case KEY_HOME:
+			int i = bufferContent->cursorPos[cursorIdentity];
+			while (i >= 0 && bufferContent->content[i] != '\n')
+				i--;
+
+			cursorPos[cursorIdentity] = i + 1;
+			break;
+		case KEY_BACKSPACE:
+			for (int i = bufferContent->cursorPos[cursorIdentity]; i < bufferContent->size && i > 0; i++) {
+				bufferContent->content[i - 1] = bufferContent->content[i];
+			}
+			bufferContent->size--;
+			break;
+		case KEY_ENTER:
+			insert('\n', bufferContent);
+			break;
+		case KEY_TAB:
+			insert('\t', bufferContent);
+			break;
+		default:
+			if (isprint(input))
+				insert(input, bufferContent);
+			break;
+	}
+}
