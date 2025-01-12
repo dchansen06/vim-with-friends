f590f071437184ca6afab93939282d92d630eb1f
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 16:45:50 -0800

    U

--- shared_memory.cpp
+++ shared_memory.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <filesystem>
 
 #include <unistd.h>
 #include <limits.h>
@@ -39,20 +40,15 @@ volatile BufferContents* getSharedMemory(string filename, bool &host)
 	const char* NAME = ("/tmp" + name).c_str();	// Later mkdir so that we can /tmp/vim-with-friends/
 
 	int shm = shm_open(NAME, O_EXCL|O_CREAT|O_RDWR, 0666);
-	cout << "SHM: " << shm << " should be >=0 for host\n";
-	if (shm >= 0) {
+
+	if (EEXIST != errno) {
 		host = true;
-		ftruncate(shm, SIZE);
-		return (volatile BufferContents*)mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm, 0);
+		cout << "Trying to do ftruncate " << ftruncate(shm, SIZE) << endl;
+		//ftruncate(shm, SIZE);
+		return (volatile BufferContents*)mmap(0, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, shm, 0);
 	} else {
 		host = false;
-
 		shm = shm_open(NAME, O_RDWR, 0666);
-
-		if (shm >= 0)
-			return (volatile BufferContents*)mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm, 0);
+		return (volatile BufferContents*)mmap(0, SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, shm, 0);
 	}
-
-	cerr << "ERROR!\n";
-	return nullptr;
 }
