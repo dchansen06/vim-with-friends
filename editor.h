84389b1d31c23741c751b848ba85d5e76eb25941
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 23:11:05 -0800

    U

--- editor.h
+++ editor.h
@@ -3,5 +3,10 @@
 #ifndef EDITOR_H
 	#define EDITOR_H
 
-	void update(volatile BufferContents *bufferContent, Cursor thisCur);
+	void insert(char character, volatile BufferContents *bufferContent, int cursorIdentity);
+	void moveDown (volatile char content[], int size, int& cursorID);
+	void moveUp (volatile char content[], int size, int& cursorID);
+	void moveRight (volatile char content[], int size, int& cursorID);
+	void moveLeft (volatile char content[], int size, int& cursorID);
+	void update(volatile BufferContents* bufferContent, int& cursorIdentity)
 #endif
