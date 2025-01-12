41dac8da9d511dd90844ddfa8543bf8cf84b0d6a
Daniel Hansen <dchansen06@gmail.com>
2025-01-11 22:25:33 -0800

    U

--- editor.cpp
+++ editor.cpp
@@ -5,9 +5,15 @@
 #include "editor.h"
 #include "shared_memory.h"
 
-void insert(char character, volatile BufferContents *bufferContent)
+void insert(char character, volatile BufferContents *bufferContent, int cursorIdentity)
 {
-	
+	for (volatile unsigned long position = bufferContent->size; i >= bufferContent->cursorPos[cursorIdentity]; i--) {
+		bufferContent->size++;
+		bufferContent->content[i+1] = bufferContent->content[i];
+	}
+
+	bufferContent->content[bufferCount->cursorPos[cursorIdentity]] = character;
+	bufferCount->cursorPos[cursorIdentity]++;
 }
 
 void update(volatile BufferContents *bufferContent, int cursorIdentity)
@@ -65,14 +71,14 @@ void update(volatile BufferContents *bufferContent, int cursorIdentity)
 			bufferContent->size--;
 			break;
 		case KEY_ENTER:
-			insert('\n', bufferContent);
+			insert('\n', bufferContent, cursorIdentity);
 			break;
 		case KEY_TAB:
-			insert('\t', bufferContent);
+			insert('\t', bufferContent, cursorIdentity);
 			break;
 		default:
 			if (isprint(input))
-				insert(input, bufferContent);
+				insert(input, bufferContent, cursorIdentity);
 			break;
 	}
 }
